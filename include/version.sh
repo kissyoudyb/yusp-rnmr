0cc31e2b12ae8981f2f323bdb990468d2b9d9ff9
danyubin <danyubin1982@163.com>
2019-01-24 16:28:58 +0800

    add version.sh

---
+++ include/version.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+Autoconf_Ver='autoconf-2.13'
+Libiconv_Ver='libiconv-1.15'
+LibMcrypt_Ver='libmcrypt-2.5.8'
+Mcypt_Ver='mcrypt-2.6.8'
+Mhash_Ver='mhash-0.9.9.9'
+Freetype_Ver='freetype-2.7'
+Freetype_New_Ver='freetype-2.9.1'
+Curl_Ver='curl-7.62.0'
+Pcre_Ver='pcre-8.42'
+Jemalloc_Ver='jemalloc-5.1.0'
+TCMalloc_Ver='gperftools-2.7'
+Libunwind_Ver='libunwind-1.2.1'
+Libicu4c_Ver='icu4c-63_1'
+Boost_Ver='boost_1_59_0'
+Boost_New_Ver='boost_1_67_0'
+Openssl_Ver='openssl-1.0.2q'
+Openssl_New_Ver='openssl-1.1.1a'
+Nghttp2_Ver='nghttp2-1.35.1'
+Luajit_Ver='LuaJIT-2.0.5'
+LuaNginxModule='lua-nginx-module-0.10.13'
+NgxDevelKit='ngx_devel_kit-0.3.0'
+Nginx_Ver='nginx-1.14.2'
+if [ "${DBSelect}" = "1" ]; then
+    Mysql_Ver='mysql-5.1.73'
+elif [ "${DBSelect}" = "2" ]; then
+    Mysql_Ver='mysql-5.5.62'
+elif [ "${DBSelect}" = "3" ]; then
+    Mysql_Ver='mysql-5.6.42'
+elif [ "${DBSelect}" = "4" ]; then
+    Mysql_Ver='mysql-5.7.24'
+elif [ "${DBSelect}" = "5" ]; then
+    Mysql_Ver='mysql-8.0.13'
+elif [ "${DBSelect}" = "6" ]; then
+    Mariadb_Ver='mariadb-5.5.62'
+elif [ "${DBSelect}" = "7" ]; then
+    Mariadb_Ver='mariadb-10.0.37'
+elif [ "${DBSelect}" = "8" ]; then
+    Mariadb_Ver='mariadb-10.1.37'
+elif [ "${DBSelect}" = "9" ]; then
+    Mariadb_Ver='mariadb-10.2.19'
+elif [ "${DBSelect}" = "10" ]; then
+    Mariadb_Ver='mariadb-10.3.11'
+fi
+
+Redis_Stable_Ver='redis-5.0.3'
+PHPRedis_Ver='redis-4.2.0'
+Memcached_Ver='memcached-1.5.12'
+Libmemcached_Ver='libmemcached-1.0.18'
