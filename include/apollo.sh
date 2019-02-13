0d8ebe0ba664431eab99689de8244b99c8cf3616
danyubin <danyubin1982@163.com>
2019-02-13 15:26:45 +0800

    install adminservice step001

--- include/apollo.sh
+++ include/apollo.sh
@@ -11,7 +11,8 @@ Install_Apollo()
 	Apollo_Create_Init_db
 	#安装configservice
 	Apollo_Install_configservice
-	#安装andminservice
+	#安装adminservice
+	Apollo_Install_adminservice
 }
 
 Apollo_Create_Init_db() {
@@ -32,6 +33,10 @@ Apollo_Create_Init_db() {
 	Do_Query "source apolloportaldb.sql"
 	echo "show database..."
 	Do_Query "show databases;"
+	echo "update ApolloConfigDB.ServerConfig Set Apollo_ConfigService_Port=${Apollo_ConfigService_Port}"
+	Do_Query "UPDATE ApolloConfigDB.ServerConfig SET Value='http://localhost:${Apollo_ConfigService_Port}/' WHERE Key='eureka.service.url';"
+	Do_Query "SELECT * FROM ApolloConfigDB.ServerConfig WHERE Key='eureka.service.url';"
+	echo "update ApolloConfigDB.ServerConfig Successful!"
 	Echo_Green "import apollo init sql success..."
 }
 
@@ -52,6 +57,27 @@ Apollo_Install_configservice(){
         Echo_Green "OK, ====== Apollo_Install_configservice Successful!!======"
     else
 		Echo_Red "Sorry, Apollo_Install_configservice failed! please check you opration and try again!"
+		return -1;
+	fi
+}
+
+Apollo_Install_adminservice(){
+    echo "====== Apollo_Install_adminservice ======"
+	Download_Files ${YUSP_Download_Mirror}/apollo-adminservice-${Apollo_Ver}-github.zip
+	unzip apollo-adminservice-${Apollo_Ver}-github.zip -d apollo-adminservice-${Apollo_Ver}
+	echo "mv apolloadminservice to /usr/local ..." 
+	rm -rf /usr/local/apollo-adminservice
+	mv apollo-adminservice-${Apollo_Ver} /usr/local/
+	cd /usr/local/apollo-adminservice-${Apollo_Ver}
+	sed -i "s#fill-in-the-correct-server#localhost#" config/application-github.properties
+	sed -i "s#FillInCorrectUser#${Apollo_DB_User}#" config/application-github.properties
+	sed -i "s#FillInCorrectPassword#${Apollo_DB_Password}#" config/application-github.properties
+	sed -i "s#SERVER_PORT=8090#SERVER_PORT=${Apollo_AdminService_Port}#" scripts/startup.sh
+	sh  /usr/local/apollo-adminservice-${Apollo_Ver}/scripts/startup.sh
+	if [ $? -eq 0 ]; then
+        Echo_Green "OK, ====== Apollo_Install_adminservice Successful!!======"
+    else
+		Echo_Red "Sorry, Apollo_Install_adminservice failed! please check you opration and try again!"
 		return -1;
 	fi
 }
