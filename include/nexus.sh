33c3cbbac041efdb8d13998f4535442ec50bd516
danyubin <danyubin1982@163.com>
2019-02-15 09:27:15 +0800

    install nexus step002

--- include/nexus.sh
+++ include/nexus.sh
@@ -11,26 +11,30 @@ Install_Nexus()
 	Nexus_Check_Install_Condition
 	#安装Nexus
 	Nexus_Install
-	#把apollo安装为linux服务
-	Nexus_Add_AutoStartup
+	#把Nexus安装为linux服务TODO
+	#Nexus_Add_AutoStartup
 }
 
-Nexus_Check_Install_Condition() {
+Nexus_Check_Install_Condition()
+{
     echo "====== Nexus_Check_Install_Condition ======"
+	source /etc/profile
 	java -version
 	if [ $? -eq 0 ]; then
 	    Echo_Green "OK, Nexus install condition check success! going on..."
 	else
 		Echo_Red "Sorry, Nexus need jdk1.8+! please install jdk1.8+ first!"
 		return -1;
+	fi
 }
 
-Nexus_Install(){
+Nexus_Install()
+{
     echo "====== Nexus_Install ======"
 	Download_Files ${YUSP_Download_Mirror}/nexus-${Nexus_Ver}-unix.tar.gz nexus-${Nexus_Ver}-unix.tar.gz
 	rm -rf /usr/local/nexus*
 	tar -xvf nexus-${Nexus_Ver}-unix.tar.gz -C ${Nexus_Install_Dir}nexus-${Nexus_Ver}	
-	cd ${Nexus_Install_Dir}nexus-${Nexus_Ver}/bin
+	${Nexus_Install_Dir}nexus-${Nexus_Ver}/bin/nexus start
 	if [ $? -eq 0 ]; then
         Echo_Green "OK, ====== Nexus_Install Successful!!======"
     else
@@ -40,6 +44,7 @@ Nexus_Install(){
 }
 
 Nexus_Add_AutoStartup(){
+	#TODO 目前如下的实现无法搞
 	cp ${Nexus_Install_Dir}nexus-${Nexus_Ver}/bin/nexus /etc/init.d/nexus
     chmod +x /etc/init.d/nexus
     echo "Add Nexus to auto startup..."
@@ -58,7 +63,7 @@ Uninstall_Nexus()
 {
     echo "You will uninstall Nexus..."
 	echo "Shutting down Nexus..."
-	/etc/init.d/nexus stop
+	${Nexus_Install_Dir}nexus-${Nexus_Ver}/bin/nexus stop
 	Remove_StartUp nexus
     echo "Delete Nexus files..."
     rm -rf ${Nexus_Install_Dir}nexus*
